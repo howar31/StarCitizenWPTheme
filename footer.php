057cd3f9f1632c76552848f4a937940a3efb966a
OpenShift System User <54fbed2afcf9333409000071@ex-std-node329.prod.rhcloud.com>
2015-03-09 10:08:25 -0400

    Add logo.

--- footer.php
+++ footer.php
@@ -11,6 +11,7 @@
 ?>
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
+		<div class="sc-logo"></div>
 		<div class="site-info">
 			<div>本網站使用 <a href="https://www.google.com/get/noto/#/family/noto-sans-hant" title="前往Google官方網站下載Google Noto Fonts字體" target="_blank">Google Noto Fonts</a> 舒適的閱讀字體，請<a href="https://www.google.com/get/noto/#/family/noto-sans-hant" title="前往Google官方網站下載Google Noto Fonts字體" target="_blank">下載</a>安裝以獲得最佳瀏覽體驗</div>
 			<div>Create and maintain by <a href="http://howar31.com" title="Howar31" target="_blank">Howar31</a> @ 2015</div>
